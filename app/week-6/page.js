1e074cb8c75fedb82a79b0ed7c6569ef86cbf84b
ElliotJ-Git <elliot.jost@edu.sait.ca>
2025-10-28 13:43:54 -0600

    Rename page for clarity

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -4,7 +4,7 @@ export default function Page(){
 
     return(
         <main className="p-4 bg-cyan-900">
-            <h1 className="text-3xl text-amber-100">Shopping List:</h1>
+            <h1 className="text-3xl text-amber-100">Shopping List (week-6):</h1>
             <div className="mt-4 space-y-4">
                 <ItemList/>
             </div>
