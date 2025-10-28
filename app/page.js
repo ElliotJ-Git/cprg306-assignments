225b0a8225482579c6a79272c66a3caaa2bf20ae
ElliotJ-Git <elliot.jost@edu.sait.ca>
2025-10-28 13:38:09 -0600

    Week-6 Complete

--- app/page.js
+++ app/page.js
@@ -11,6 +11,7 @@ export default function Assignments() {
           <li><Link className="text-cyan-600 underline" href="./week-3/">Assignment3 - Week 3</Link></li>
           <li><Link className="text-cyan-600 underline" href="./week-4/">Assignment4 - Week 4</Link></li>
           <li><Link className="text-cyan-600 underline" href="./week-5/">Assignment5 - Week 5</Link></li>
+          <li><Link className="text-cyan-600 underline" href="./week-6/">Assignment6 - Week 6</Link></li>
         </ul>
 
       </main>
---
+++ app/week-6/page.js
@@ -0,0 +1,13 @@
+import ItemList from "./item-list.js";
+
+export default function Page(){
+
+    return(
+        <main className="p-4 bg-cyan-900">
+            <h1 className="text-3xl text-amber-100">Shopping List:</h1>
+            <div className="mt-4 space-y-4">
+                <ItemList/>
+            </div>
+        </main>
+    );
+}
